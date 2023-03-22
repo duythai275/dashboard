aca8b0526875d945c9d0fe44807a37306596be4d
dangtrungdung1701 <dangtrungdung1701@gmail.com>
2023-03-22 15:28:40 +0700

    add w1 in disease dashboard

--- configs/gdpm/frontend/dashboards/DiseaseDashBoard/index.jsx
+++ configs/gdpm/frontend/dashboards/DiseaseDashBoard/index.jsx
@@ -1,18 +1,25 @@
 import WidgetContainer from "@/components/WidgetContainer/WidgetContainer";
+import useMetadataStore from "@/state/metadata";
 import ReactGridLayout from "react-grid-layout";
 import { useTranslation } from "react-i18next";
+import { shallow } from "zustand/shallow";
 import Widget1 from "./widgets/Widget1";
 
 const DiseaseDashboard = ({ disease }) => {
   const { t } = useTranslation();
-  console.log("aasdasd11112321312");
+  const { ouGroups } = useMetadataStore(
+    (state) => ({
+      ouGroups: state.ouGroups,
+    }),
+    shallow
+  );
+
   return (
     <ReactGridLayout
       isDraggable={false}
       layout={[
-        { i: "1", x: 0, y: 0, w: 5.95, h: 30 },
-        { i: "2", x: 5.95, y: 0, w: 5.95, h: 30 },
-        { i: "3", x: 0, y: 30, w: 11.9, h: 30 },
+        { i: "1", x: 0, y: 0, w: 11.9, h: 50 },
+        { i: "2", x: 10, y: 50, w: 11.9, h: 30 },
       ]}
       cols={12}
       rowHeight={1}
@@ -22,16 +29,23 @@ const DiseaseDashboard = ({ disease }) => {
         key="1"
         dashboardIndex={0}
         widgetIndex={0}
-        childrenWidgets={[
-          {
-            title: t("widget1Title"),
-            widget: <Widget1 code={disease} ou={"S3kaCiYIP4B"} />,
-          },
-          {
-            title: t("widget1Title1"),
-            widget: <Widget1 code={disease} ou={"S3kaCiYIP4B"} />,
-          },
-        ]}
+        childrenWidgets={ouGroups[3].organisationUnits.map((province) => {
+          return {
+            title: province.name,
+            widget: <Widget1 code={disease} ou={province.id} />,
+          };
+        })}
+      />
+      <WidgetContainer
+        key="2"
+        dashboardIndex={0}
+        widgetIndex={0}
+        childrenWidgets={ouGroups[3].organisationUnits.map((province) => {
+          return {
+            title: province.name,
+            widget: <Widget1 code={disease} ou={province.id} />,
+          };
+        })}
       />
       {/* <WidgetContainer
         key="2"
--- configs/gdpm/frontend/dashboards/DiseaseDashBoard/widgets/Widget1.jsx
+++ configs/gdpm/frontend/dashboards/DiseaseDashBoard/widgets/Widget1.jsx
@@ -1,23 +1,58 @@
 import MultitypeChart from "@/components/Widgets/MultitypeChart";
+import withWidgetChildrenLoader from "@/hocs/WidgetContainer/withWidgetChildrenLoader";
 import { pull } from "@/utils/fetch";
 import { useEffect } from "react";
 
-const Widget1 = ({ code, ou }) => {
-  const getData = async () => {
-    try {
-      const result = await pull(
-        `/api/analytics/events/query/PO07dgbJCgr.json?dimension=pe:2023W1;2023W2;2023W3;2023W4;2023W5;2023W6;2023W7;2023W8;2023W9;2023W10;2023W11;2023W12;2023W13;2023W14;2023W15;2023W16;2023W17;2023W18;2023W19;2023W20;2023W21;2023W22;2023W23;2023W24;2023W25;2023W26;2023W27;2023W28;2023W29;2023W30;2023W31;2023W32;2023W33;2023W34;2023W35;2023W36;2023W37;2023W38;2023W39;2023W40;2023W41;2023W42;2023W43;2023W44;2023W45;2023W46;2023W47;2023W48;2023W49;2023W50;2023W51;2023W52;2022W1;2022W2;2022W3;2022W4;2022W5;2022W6;2022W7;2022W8;2022W9;2022W10;2022W11;2022W12;2022W13;2022W14;2022W15;2022W16;2022W17;2022W18;2022W19;2022W20;2022W21;2022W22;2022W23;2022W24;2022W25;2022W26;2022W27;2022W28;2022W29;2022W30;2022W31;2022W32;2022W33;2022W34;2022W35;2022W36;2022W37;2022W38;2022W39;2022W40;2022W41;2022W42;2022W43;2022W44;2022W45;2022W46;2022W47;2022W48;2022W49;2022W50;2022W51;2022W52&dimension=ou:${ou}&dimension=GIdhyQcAihV.Du5ydup8qQf:IN:${code}&stage=GIdhyQcAihV&displayProperty=NAME&totalPages=false&outputType=EVENT&desc=eventdate&outputIdScheme=UID&paging=false`
-      );
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  useEffect(() => {
-    getData();
-  }, []);
+const Widget1 = ({ setLoading, code, ou }) => {
+  // const getData = async () => {
+  //   try {
+  //     const result = await pull(
+  //       `/api/analytics/events/query/PO07dgbJCgr.json?dimension=pe:2023W1;2023W2;2023W3;2023W4;2023W5;2023W6;2023W7;2023W8;2023W9;2023W10;2023W11;2023W12;2023W13;2023W14;2023W15;2023W16;2023W17;2023W18;2023W19;2023W20;2023W21;2023W22;2023W23;2023W24;2023W25;2023W26;2023W27;2023W28;2023W29;2023W30;2023W31;2023W32;2023W33;2023W34;2023W35;2023W36;2023W37;2023W38;2023W39;2023W40;2023W41;2023W42;2023W43;2023W44;2023W45;2023W46;2023W47;2023W48;2023W49;2023W50;2023W51;2023W52;2022W1;2022W2;2022W3;2022W4;2022W5;2022W6;2022W7;2022W8;2022W9;2022W10;2022W11;2022W12;2022W13;2022W14;2022W15;2022W16;2022W17;2022W18;2022W19;2022W20;2022W21;2022W22;2022W23;2022W24;2022W25;2022W26;2022W27;2022W28;2022W29;2022W30;2022W31;2022W32;2022W33;2022W34;2022W35;2022W36;2022W37;2022W38;2022W39;2022W40;2022W41;2022W42;2022W43;2022W44;2022W45;2022W46;2022W47;2022W48;2022W49;2022W50;2022W51;2022W52&dimension=ou:${ou}&dimension=GIdhyQcAihV.Du5ydup8qQf:IN:${code}&stage=GIdhyQcAihV&displayProperty=NAME&totalPages=false&outputType=EVENT&desc=eventdate&outputIdScheme=UID&paging=false`
+  //     );
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   getData();
+  // }, [ou]);
 
-  return <div>sadasdasdasdasdasd</div>;
+  const weeks = () => {
+    let weeks = [];
+    for (let i = 1; i <= 52; i++) {
+      weeks.push(`W${i}`);
+    }
+    return weeks;
+  };
+
+  return (
+    <MultitypeChart
+      data={{
+        labels: weeks(),
+        datasets: [
+          {
+            type: "line",
+            label: new Date().getFullYear(),
+            borderColor: "rgb(255, 99, 132)",
+            borderWidth: 2,
+            fill: false,
+            data: weeks().map(() => Math.floor(Math.random() * (1000 - 0))),
+          },
+          {
+            type: "bar",
+            label: new Date().getFullYear() - 1,
+            backgroundColor: "rgb(75, 192, 192)",
+            data: weeks().map(() => Math.floor(Math.random() * (1000 - 0))),
+            borderColor: "white",
+            borderWidth: 2,
+          },
+        ],
+      }}
+    />
+  );
 };
 
-export default Widget1;
+export default withWidgetChildrenLoader(Widget1);
+
+const weeks = [];
