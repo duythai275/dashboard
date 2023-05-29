5dae9ecb67eb717519c6b7a60892e13af6ddccbe
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2023-05-29 15:27:39 +0700

    remove legend for cluster

--- src/components/Widgets/ClusterMap.jsx
+++ src/components/Widgets/ClusterMap.jsx
@@ -94,7 +94,7 @@ const ClusterMap = (props) => {
   return (
     <MapContainer scrollWheelZoom={false} dragging={false}>
       <ClusterLayer {...props} />
-      <LegendLayer {...props} />
+      {/* <LegendLayer {...props} /> */}
       <TileLayer
         attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`}
         url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
